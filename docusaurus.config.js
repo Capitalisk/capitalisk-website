d74a705ca8080392b387f116b43d566194113131
Maarten Coppens <martinuske@gmail.com>
2021-04-02 08:58:16 -0300

    Added logo to the footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,6 +105,11 @@ module.exports = {
           ],
         },
       ],
+      logo: {
+        alt: 'Capitalisk',
+        src: 'img/logo-grey.png',
+        href: 'https://capitalisk.com',
+      },
       copyright: `Copyright © ${new Date().getFullYear()} Capitalisk.`,
     },
   },
