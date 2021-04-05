61112675dc483c9ed555ef0a0edc3b3e94e2d3c1
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-04-05 23:09:09 +0200

    Fix text

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,7 +115,7 @@ function Home() {
             >
               <h1 className={styles.heroSubtitle}>{siteConfig.tagline}</h1>
               <div className='text-white'>
-                Capitalisk is a highly energy-efficient Delegated Proof of Stake (DPoS) blockchain
+                Capitalisk is an energy-efficient Delegated Proof of Stake (DPoS) blockchain
                 which is resistant to quantum computing attacks.
                 Capitalisk is centered around Lisk DEX; a technology which allows any group of people to launch
                 decentralized markets for trading across any blockchain within the ecosystem.
