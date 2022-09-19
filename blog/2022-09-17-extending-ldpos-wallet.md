2742ba1c32df91336647974917f3bb74b1dfed54
Maarten Coppens <martinuske@gmail.com>
2022-09-19 13:40:34 -0300

    add blogpost

---
+++ blog/2022-09-17-extending-ldpos-wallet.md
@@ -0,0 +1,47 @@
+--
+slug: upcoming-months
+title: Extending the LDPoS ecosystem for broader side-chain adoption
+author: Maarten Coppens
+author_title: Co-Founder @ Capitalisk
+author_url: https://github.com/maarteNNNN
+author_image_url: https://avatars.githubusercontent.com/u/14275291?v=4
+tags: [update, feature]
+---
+
+### Capitalisk's strategy
+
+Capitalisk's future strategy is centered around reducing the friction involved in adopting cryptocurrency tokens within its ecosystem; both on the consumer side and producer side.
+
+On the consumer side, this means:
+
+- Making it easier to send tokens.
+- Making it easier to manage blockchain wallets/accounts.
+
+On the producer side, this means:
+
+- Making it easier to launch new tokens/blockchains.
+- Making it easier to receive tokens as payment.
+- Setting up financial incentives for businesses to accept tokens as payment.
+
+### LDPOS-Wallet
+
+Over the following few months we plan to invest heavily on adding features to the [`ldpos-wallet`](https://github.com/Capitalisk/ldpos-wallet) application to better support side-chains. Although the current state of the LDPoS wallet allows that, the current flow adds too much friction for the user.
+At the time developing the app, the focus was primarily on the Capitalisk chain. Over the past few months, we have been discussing the technical direction of the `ldpos-wallet` app. A point that we as founders all agree on is that Capitalisk is and always will be an ecosystem that allows any groups of people to create their own separate blockchains and markets with full autonomy. We believe and hope that the LDPoS technology which underpins Capitalisk will become widely adopted and that developers will soon realize its potential.
+
+One of the areas we plan to focus on will be to improve the app's ability to handle QR codes for swift and easy transactions. This will essentially allow any camera device to scan a QR code which links to a pre-filled transaction. This feature is being discussed [here](https://github.com/Capitalisk/ldpos-wallet/issues/113). Any comments and ideas are welcome.
+
+### Capitalisk's future products
+
+Aside from the `ldpos-wallet`, we have been discussing making a marketplace for projects. As we put side-chain projects in the center of our ecosystem we have been brainstorming about creating a place that people can post projects on. One thought has been that investors can invest in said projects in exchange are rewarded with a portion of said token. We ideally aim to deploy a token in a matter of a few minutes, by using a default batch of genesis delegates. However if the user so desires, they can provide their own nodes, this however means that the setup process will be prolonged.
+
+Due to our new marketplace ambition we plan to provide consultancy in the initial stages. Any project can get in contact with us to implement any app or idea that involves our technology. We will make a page on our website with a contact form for that soon.
+
+In the future we plan to add customizability to the `ldpos-wallet` so that any side-chain can brand the application as their own.
+
+### Ark implementation in LDEX
+
+We have collaborated with [Ark](https://ark.io/) to implement their token in the DEX. We are aiming to launch this month.
+
+On a side-note we are looking for someone with sales and marketing experience that wants to be a part of the project. Feel free to reach out to me on [martinuske@gmail.com](mailto:martinuske@gmail.com) or by joining our [Discord](https://discord.gg/Nrf33ck3Qq).
+
+Stay tuned, more to come.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,165 +1,172 @@
 module.exports = {
-    title: 'Capitalisk | Quantum Resistant Cryptocurrency Ecosystem',
-    tagline: 'Decentralized free markets',
-    url: 'https://capitalisk.com',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'Capitalisk', // Usually your GitHub org/user name.
-    projectName: 'capitalisk-site', // Usually your repo name.
-    themeConfig: {
-        navbar: {
-            logo: {
-                alt: 'Capitalisk',
-                src: 'img/capitalisk-logo-horizontal.png',
-                srcDark: 'img/capitalisk-logo-horizontal-white.png',
-            },
-            items: [
-                {
-                    to: 'docs/',
-                    activeBasePath: 'docs',
-                    label: 'DOCS',
-                    position: 'right',
-                    className: 'nav-item',
-                },
-                {
-                    to: 'whitepaper',
-                    label: 'WHITEPAPER',
-                    position: 'right',
-                    className: 'nav-item',
-                },
-                // {
-                //   to: 'blog/',
-                //   activeBasePath: 'blog',
-                //   label: 'BLOG',
-                //   position: 'right',
-                //   className: 'nav-item',
-                // },
-                {
-                    to: 'wallet',
-                    label: 'CLIENTS',
-                    position: 'right',
-                    className: 'nav-item',
-                },
-                {
-                    to: 'https://capitalisk.com/wallet/ldpos/#/',
-                    label: 'WALLET',
-                    position: 'right',
-                    className: 'external-nav-item',
-                },
-                {
-                    href: 'https://ldex.trading',
-                    label: 'LDEX',
-                    position: 'right',
-                    className: 'external-nav-item',
-                },
-            ],
+  title: 'Capitalisk | Quantum Resistant Cryptocurrency Ecosystem',
+  tagline: 'Decentralized free markets',
+  url: 'https://capitalisk.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Capitalisk', // Usually your GitHub org/user name.
+  projectName: 'capitalisk-site', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: 'Capitalisk',
+        src: 'img/capitalisk-logo-horizontal.png',
+        srcDark: 'img/capitalisk-logo-horizontal-white.png',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'DOCS',
+          position: 'right',
+          className: 'nav-item',
         },
-        footer: {
-            style: 'light',
-            links: [
-                {
-                    title: 'Products',
-                    items: [
-                        {
-                            label: 'Wallet',
-                            to: 'wallet/',
-                        },
-                        {
-                            label: 'Exchange',
-                            to: 'https://ldex.trading',
-                        },
-                    ],
-                },
-                {
-                    title: 'Developers',
-                    items: [
-                        {
-                            label: 'Documentation',
-                            to: 'docs/',
-                        },
-                        {
-                            label: 'Github',
-                            to: 'https://github.com/Capitalisk',
-                        },
-                        {
-                            label: 'Whitepaper',
-                            to: 'whitepaper/'
-                        },
-                    ],
-                },
-                {
-                    title: 'Community',
-                    items: [
-                        // {
-                        //   label: 'Stack Overflow',
-                        //   href: 'https://stackoverflow.com/questions/tagged/capitalisk',
-                        // },
-                        {
-                            label: 'Discord',
-                            href: 'https://discord.gg/Nrf33ck3Qq',
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/CapitaliskHQ',
-                        },
-                        {
-                            label: 'Telegram',
-                            href: 'https://t.me/Capitalisk',
-                        },
-                        {
-                            label: 'Reddit',
-                            href: 'https://www.reddit.com/r/Capitalisk/',
-                        },
-                    ],
-                },
-                {
-                    title: 'About',
-                    items: [
-                        {
-                            label: 'Blog',
-                            to: 'blog',
-                        },
-                        {
-                            label: 'FAQ\'s',
-                            to: 'wallet/',
-                        }
-                    ],
-                },
-            ],
-            logo: {
-                alt: 'Capitalisk',
-                src: 'img/logo-grey.png',
-            },
-            copyright: `Copyright © ${new Date().getFullYear()} Capitalisk.`,
+        {
+          to: 'whitepaper',
+          label: 'WHITEPAPER',
+          position: 'right',
+          className: 'nav-item',
         },
+        // {
+        //   to: 'blog/',
+        //   activeBasePath: 'blog',
+        //   label: 'BLOG',
+        //   position: 'right',
+        //   className: 'nav-item',
+        // },
+        {
+          to: 'wallet',
+          label: 'APPS',
+          position: 'right',
+          className: 'nav-item',
+        },
+        {
+          to: 'blog',
+          label: 'BLOG',
+          position: 'right',
+          className: 'nav-item',
+        },
+        {
+          to: 'https://capitalisk.com/wallet/ldpos/#/',
+          label: 'WALLET',
+          position: 'right',
+          className: 'external-nav-item',
+        },
+        {
+          href: 'https://ldex.trading',
+          label: 'LDEX',
+          position: 'right',
+          className: 'external-nav-item',
+        },
+      ],
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Products',
+          items: [
             {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    // editUrl:
-                    //   'https://github.com/facebook/docusaurus/edit/master/website/',
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // editUrl:
-                    //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.scss'),
-                },
+              label: 'Wallet',
+              to: 'wallet/',
             },
-        ],
+            {
+              label: 'Exchange',
+              to: 'https://ldex.trading',
+            },
+          ],
+        },
+        {
+          title: 'Developers',
+          items: [
+            {
+              label: 'Documentation',
+              to: 'docs/',
+            },
+            {
+              label: 'Github',
+              to: 'https://github.com/Capitalisk',
+            },
+            {
+              label: 'Whitepaper',
+              to: 'whitepaper/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/capitalisk',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/Nrf33ck3Qq',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/CapitaliskHQ',
+            },
+            {
+              label: 'Telegram',
+              href: 'https://t.me/Capitalisk',
+            },
+            {
+              label: 'Reddit',
+              href: 'https://www.reddit.com/r/Capitalisk/',
+            },
+          ],
+        },
+        {
+          title: 'About',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: "FAQ's",
+              to: 'wallet/',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Capitalisk',
+        src: 'img/logo-grey.png',
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Capitalisk.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.scss'),
+        },
+      },
     ],
-    plugins: ['docusaurus-plugin-sass'],
-    customFields: {
-        metaDescription: 'Capitalisk is an energy-efficient DPoS blockchain. Allows any group of people to launch a decentralized cryptocurrency.' +
-            ' Easily integrate crypto markets with Capitalisk DEX ecosystem. Resistant to quantum computing attacks'
-    }
+  ],
+  plugins: ['docusaurus-plugin-sass'],
+  customFields: {
+    metaDescription:
+      'Capitalisk is an energy-efficient DPoS blockchain. Allows any group of people to launch a decentralized cryptocurrency.' +
+      ' Easily integrate crypto markets with Capitalisk DEX ecosystem. Resistant to quantum computing attacks',
+  },
 };
